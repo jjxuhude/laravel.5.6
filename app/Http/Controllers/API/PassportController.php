9ee091785088e3e8e08c79e7fd96a24fd4ac237c
jjxuhude <jjxuhuade@163.com>
2018-05-04 15:58:28 +0800

    1

--- app/Http/Controllers/API/PassportController.php
+++ app/Http/Controllers/API/PassportController.php
@@ -2,9 +2,9 @@
 namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
-use App\User;
 use Illuminate\Support\Facades\Auth;
 use Validator;
+use App\Model\User;
 
 class PassportController extends Controller
 {
