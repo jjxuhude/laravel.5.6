5b8f3dcf37da5f2eb9ad0c611feaf6f6e0e52d8d
jjxuhude <jjxuhuade@163.com>
2018-05-04 16:04:53 +0800

    1

--- app/Http/Controllers/API/PassportController.php
+++ app/Http/Controllers/API/PassportController.php
@@ -48,10 +48,8 @@ class PassportController extends Controller
         $input = $request->all();
         $input['password'] = bcrypt($input['password']);
         $user = User::create($input);
-        $success['token'] =  $user->createToken('MyApp')->accessToken;
-        $success['name'] =  $user->name;
-        
-        return response()->json(['success'=>$success], $this->successStatus);
+        return response()->json(['success'=>'successs'], $this->successStatus);
+      
     }
     
     /**
