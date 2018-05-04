75874b8438ea91d0910fe994ea9bf53571f0ec07
jjxuhude <jjxuhuade@163.com>
2018-05-04 11:51:37 +0800

    1

--- app/Services/RouteConfig.php
+++ app/Services/RouteConfig.php
@@ -18,7 +18,7 @@ class RouteConfig
         foreach ($methods as $method) {
             $route = strtolower(strstr(basename($method->class), 'Controller', true));
             
-            preg_match_all('/[\\\](.+?)/', $method->class,$match);
+            preg_match_all('/[\\\](.+)/', $method->class,$match);
             dump($match);
             
             if ($method->name == 'index') {
