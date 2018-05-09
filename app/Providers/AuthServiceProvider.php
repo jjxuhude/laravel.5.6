952e67eadd4c9ca104524a6c1ef00b5de4b7e020
jjxuhude <jjxuhuade@163.com>
2018-05-09 17:05:25 +0800

    1

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -35,9 +35,9 @@ class AuthServiceProvider extends ServiceProvider
         //隐式授权令牌
         Passport::enableImplicitGrant();
         
-        Passport::tokensCan([
-            'place-orders' => 'Place orders',
-            'check-status' => 'Check order status',
-        ]);
+//         Passport::tokensCan([
+//             'place-orders' => 'Place orders',
+//             'check-status' => 'Check order status',
+//         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,22 @@ Route::post('login', 'API\PassportController@login');
 Route::post('register', 'API\PassportController@register');
 Route::get('getDetails', 'API\PassportController@getDetails');
 
+//客户端凭证授权令牌
+Route::get('/credentials', function(Request $request) {
+    $guzzle = new GuzzleHttp\Client;
+    
+    $response = $guzzle->post(url('oauth/token'), [
+        'form_params' => [
+            'grant_type' => 'client_credentials',
+            'client_id' => '1',
+            'client_secret' => 'ml3KXBt88wc6Lg5GEBBUVsMPTN1zHOO8E6u83Fnk',
+            'scope' => '*',
+        ],
+    ]);
+    
+    return json_decode((string) $response->getBody(), true);
+})->middleware('client');
+
 //客户端代码
 Route::get('/redirect', function (){
     $query = http_build_query([
